03100e65e3637a18a0a5affc7c16f07139026e89
Christopher Ducey <chrisducey01@me.com>
2019-11-04 13:49:23 -0700

    Added model and controller for adding, updating burger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,12 +2,25 @@ const burger = require("../models/burger");
 const express = require("express");
 const router = express.Router();
 
-router.get("/api/burgers", (req,res) => {
+router.get("/api/burgers", (req, res) => {
     burger.getAllBurgers(function (data) {
         console.log(data);
         res.json(data);
     });
-}
-);
+});
+
+router.post("/api/burgers/", (req, res) => {
+    burger.addBurger(req.body.burger, req.body.devoured, (data) =>{
+        console.log(data);
+        res.json({id: data.insertId});
+    });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+    burger.updateBurger(req.params.id, {burger_name: req.body.burger_name, devoured: req.body.devoured}, (data) => {
+        console.log(data);
+        res.json(data);
+    });
+});
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -4,7 +4,17 @@ const burger={
     getAllBurgers: function(cb){
         orm.selectAll("burgers", function(results){
             cb(results);
-        })
+        });
+    },
+    addBurger: function(burgerName, devoured=false, cb){
+        orm.insertOne("burgers",{burger_name: burgerName, devoured: devoured}, function(results){
+            cb(results);
+        });
+    },
+    updateBurger: function(burgerId, updateObj, cb){
+        orm.updateOne("burgers", updateObj, "id", burgerId, function(results){
+            cb(results);
+        });     
     }
 };
 
